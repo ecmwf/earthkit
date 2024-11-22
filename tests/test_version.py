ef288089327e2f557e5debe90e9885812051d2c6
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2024-11-22 12:03:33 +0000

    Split tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -36,23 +36,6 @@ def test_earthkit_data_version_2():
     assert VERSION_RX.match(data.__version__) is not None
 
 
-def test_earthkit_data_import():
-    import earthkit.data  # noqa
-    from earthkit.data import from_source  # noqa
-
-
-def test_earthkit_data_obj_1():
-    import earthkit.data
-
-    assert callable(earthkit.data.from_source)
-
-
-def test_earthkit_data_obj_2():
-    from earthkit import data
-
-    assert callable(data.from_source)
-
-
 def test_earthkit_geo_version_1():
     import earthkit.geo  # noqa
 
@@ -63,23 +46,6 @@ def test_earthkit_geo_version_2():
     from earthkit import geo  # noqa
 
     assert VERSION_RX.match(geo.__version__) is not None
-
-
-def test_earthkit_geo_import():
-    import earthkit.geo  # noqa
-    from earthkit.geo import nearest_point_kdtree  # noqa
-
-
-def test_earthkit_geo_obj_1():
-    import earthkit.geo
-
-    assert callable(earthkit.geo.nearest_point_kdtree)
-
-
-def test_earthkit_geo_obj_2():
-    from earthkit import geo
-
-    assert callable(geo.nearest_point_kdtree)
 
 
 def test_earthkit_meteo_version_1():
@@ -94,23 +60,6 @@ def test_earthkit_meteo_version_2():
     assert VERSION_RX.match(meteo.__version__) is not None
 
 
-def test_earthkit_meteo_import():
-    import earthkit.meteo  # noqa
-    from earthkit.meteo import thermo  # noqa
-
-
-def test_earthkit_meteo_obj_1():
-    import earthkit.meteo
-
-    assert callable(earthkit.meteo.thermo.potential_temperature)
-
-
-def test_earthkit_meteo_obj_2():
-    from earthkit import meteo
-
-    assert callable(meteo.thermo.potential_temperature)
-
-
 def test_earthkit_plots_version_1():
     import earthkit.plots  # noqa
 
@@ -123,23 +72,6 @@ def test_earthkit_plots_version_2():
     assert VERSION_RX.match(plots.__version__) is not None
 
 
-def test_earthkit_plots_import():
-    import earthkit.plots  # noqa
-    from earthkit.plots import quickmap  # noqa
-
-
-def test_earthkit_plots_obj_1():
-    import earthkit.plots
-
-    assert callable(earthkit.plots.quickmap.plot)
-
-
-def test_earthkit_plots_obj_2():
-    from earthkit import plots
-
-    assert callable(plots.quickmap.plot)
-
-
 def test_earthkit_regrid_version_1():
     import earthkit.regrid  # noqa
 
@@ -150,23 +82,6 @@ def test_earthkit_regrid_version_2():
     from earthkit import regrid  # noqa
 
     assert VERSION_RX.match(regrid.__version__) is not None
-
-
-def test_earthkit_regrid_import():
-    import earthkit.regrid  # noqa
-    from earthkit.regrid import interpolate  # noqa
-
-
-def test_earthkit_regrid_obj_1():
-    import earthkit.regrid
-
-    assert callable(earthkit.regrid.interpolate)
-
-
-def test_earthkit_regrid_obj_2():
-    from earthkit import regrid
-
-    assert callable(regrid.interpolate)
 
 
 def test_earthkit_time_version_1():
@@ -182,25 +97,6 @@ def test_earthkit_time_version_2():
     assert VERSION_RX.match(time.__version__) is not None
 
 
-def test_earthkit_time_import():
-    import earthkit.time  # noqa
-    from earthkit.time import DailySequence  # noqa
-
-
-def test_earthkit_time_obj_1():
-    import earthkit.time
-
-    x = earthkit.time.DailySequence()
-    assert x is not None
-
-
-def test_earthkit_time_obj_2():
-    from earthkit import time
-
-    x = time.DailySequence()
-    assert x is not None
-
-
 def test_earthkit_transforms_version_1():
     import earthkit.transforms  # noqa
 
@@ -211,20 +107,3 @@ def test_earthkit_transforms_version_2():
     from earthkit import transforms  # noqa
 
     assert VERSION_RX.match(transforms.__version__) is not None
-
-
-def test_earthkit_transforms_import():
-    import earthkit.transforms  # noqa
-    from earthkit.transforms import aggregate  # noqa
-
-
-def test_earthkit_transforms_obj_1():
-    import earthkit.transforms
-
-    assert callable(earthkit.transforms.aggregate.spatial.masks)
-
-
-def test_earthkit_transforms_obj_2():
-    from earthkit import transforms
-
-    assert callable(transforms.aggregate.spatial.masks)
